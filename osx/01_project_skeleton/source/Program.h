96d2d8fe908a222f2ce7e18c7a018ef080ac3f79
Air Tayork <airtayork@gmail.com>
2014-03-09 09:31:50 +0800

    add Program and main

--- osx/01_project_skeleton/source/Program.h
+++ osx/01_project_skeleton/source/Program.h
@@ -16,3 +16,50 @@
  limitations under the License.
  */
 
+#pragma once
+
+#include "Shader.h"
+#include <vector>
+
+namespace tdogl {
+    
+    /**
+     Represents an OpenGL program made by linking shaders.
+     */
+    class Program {
+    public:
+        /**
+         Creates a program by linking a list of tdogl::Shader objects
+         
+         @param shaders  The shaders to link together to make the program
+         
+         @throw std::exception if an error occurs.
+         
+         @see tdogl::Shader
+         */
+        Program(const std::vector<Shader>& shaders);
+        ~Program();
+        
+        /**
+         @result The program's object is, as returned form glCreateProgram.
+         */
+        GLuint object() const;
+        
+        /**
+         @result The attribute index for the given name, as returned from glGetAttribLocation.
+         */
+        GLint attrib(const GLchar* attribName) const;
+        
+        /**
+         @result The uniform index for the given name, as returned from glGetUniformLocation.
+         */
+        GLint uniform(const GLchar* uniformName) const;
+        
+    private:
+        GLuint _object;
+        
+        //copying disabled
+        Program(const Program&);
+        const Program& operator=(const Program&);
+    };
+}
